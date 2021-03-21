380947eff82afa7e108cf1f90be340a2b4365f61
Ivan Barbić <ivan.barbic.1994@gmail.com>
2021-03-21 17:01:16 +0100

    First task done

--- Vjezba1/Zad1/Program.cs
+++ Vjezba1/Zad1/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Vjezba1
+
+
+namespace Zad1
 {
     class Program
     {
