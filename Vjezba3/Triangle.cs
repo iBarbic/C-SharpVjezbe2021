9768977fcb7aca81ab23893b75e413c3fa4f6c8b
Ivan Barbić <ivan.barbic.1994@gmail.com>
2021-04-03 14:54:23 +0200

    Third task done

---
+++ Vjezba3/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labs
+{
+    class Triangle:Shape
+
+    {
+        private double side;
+        
+        public Triangle(double a)
+        {
+            side = a;
+            xPos = DataModel.getNewXPos();
+            yPos = DataModel.getNewYPos();
+        }
+
+        public override double getArea()
+        {
+            return ((Math.Sqrt(3) / 4) * (side * side));
+        }
+        public override double getPerimeter()
+        {
+            return 3 * side;
+        }
+
+        public override void printData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("My type: " + this.GetType());
+            Console.WriteLine("Side of triangle = " + side);
+            Console.WriteLine("X position = " + xPos);
+            Console.WriteLine("Y position = " + yPos);
+        }
+
+
+    }
+
+   
+}
