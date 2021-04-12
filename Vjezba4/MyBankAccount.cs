f1cb6d583f2d3aab773a67f9fac65c4fa7fbda37
Ivan Barbić <57228466+iBarbic@users.noreply.github.com>
2021-04-12 18:17:34 +0200

    4 task still working on it

---
+++ Vjezba4/MyBankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba4
+{
+    public class MyBankAccount 
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Oib { get; set; }
+        public DateTime Date { get; set; }
+        public string Sex { get; set; }
+        public string AccountNumber { get; set; }
+        public string Amount { get; set; }
+        public string AccountType { get; set; }
+
+        public void Reset()
+        {
+            Name = "";
+        }
+    }
+}
